537d43eec0b352282cb830ddc7c3359696e2e0df
khanhha <khanhhh89@gmail.com>
2017-12-01 11:02:48 +0700

    test all oh objects

--- src/pclwrap/examples/octree_test.py
+++ src/pclwrap/examples/octree_test.py
@@ -1,54 +1,78 @@
+import os.path
 import numpy as np
-import random
 import pcl
+
 from objsimple2 import objreader
 
-# load point and normals
-reader = objreader.read('.\\lucy_none-Slice-55_center_vn_normal.obj')
-points = np.zeros((len(reader.vv), 3), dtype=np.float32)
-normals = np.zeros((len(reader.vv),4), dtype=np.float32)
-for v, vdata in enumerate(reader.vv):
-    for i, scalar in enumerate(vdata):
-        points[v][i] = scalar
+basepath = 'G:\\Projects\\Oh\data\\test_data\\'
+filenames = {'normal_lucy_none-Slice-54_center_vn.obj',
+             'normal_lucy_none-Slice-55_center_vn.obj',
+             'normal_lucy_none-Slice-56_center_vn.obj',
+             'normal_lucy_none-Slice-57_center_vn.obj',
+             'normal_lucy_tshirt-Slice-54_center_vn.obj',
+             'normal_lucy_tshirt-Slice-55_center_vn.obj',
+             'normal_lucy_tshirt-Slice-56_center_vn.obj',
+             'normal_lucy_tshirt-Slice-57_center_vn.obj',
+             'normal_lucy_standard_tee_repaired.obj',
+             'normal_lucy_none_repaired.obj',
+             'normal_oh_none_repaired.obj'}
 
-for n, ndata in enumerate(reader._vn):
-    for i, scalar in enumerate(ndata):
-        normals[n][i] = scalar
-    normals[n][3] = 1.0
+for name in filenames:
+    filepath = basepath + name
+    if os.path.exists(filepath) == False:
+        print('Error file does not exist: ' + filepath)
+        continue
 
-# create point cloud cloud
-cloudpoint = pcl.PointCloud()
-cloudpoint.from_array(points)
-cloudnormal = pcl.PointCloud_Normal()
-cloudnormal.from_array(normals)
+    reader = objreader.read(filepath)
+    points = np.zeros((len(reader.vv), 3), dtype=np.float32)
+    normals = np.zeros((len(reader.vv),4), dtype=np.float32)
+    for v, vdata in enumerate(reader.vv):
+        for i, scalar in enumerate(vdata):
+            points[v][i] = scalar
 
-# build tree based on normal and max per leaf node
-octreeNormal = pcl.OctreePointCloudNormal(0.1)
-octreeNormal.set_input_cloud(cloudpoint)
-octreeNormal.set_input_normal_cloud(cloudnormal)
-octreeNormal.enable_dynamic_depth(100)
-octreeNormal.set_leaf_normal_threshold(0.8)
-octreeNormal.add_points_from_input_cloud()
+    for n, ndata in enumerate(reader._vn):
+        for i, scalar in enumerate(ndata):
+            normals[n][i] = scalar
+        normals[n][3] = 1.0
 
-# test search method
-searchPoint = cloudpoint[50]
-[ind, sqdist] = octreeNormal.radius_search(searchPoint, 30, 20)
+    # create point cloud cloud
+    cloudpoint = pcl.PointCloud()
+    cloudpoint.from_array(points)
+    cloudnormal = pcl.PointCloud_Normal()
+    cloudnormal.from_array(normals)
 
-# nearest k search
-[ind_1, sqdist_1] = octreeNormal.nearest_k_search_for_a_point(searchPoint, 20)
+    # build tree based on normal and max per leaf node
+    octreeNormal = pcl.OctreePointCloudNormal(0.1)
+    octreeNormal.set_input_cloud(cloudpoint)
+    octreeNormal.set_input_normal_cloud(cloudnormal)
+    octreeNormal.enable_dynamic_depth(100)
+    octreeNormal.set_leaf_normal_threshold(0.8)
+    octreeNormal.add_points_from_input_cloud()
 
-# nearest point
-[idx_2, dst_2] = octreeNormal.approx_nearest_search(cloudpoint[50]);
-assert idx_2 == 50
+    print ('search result of object: ' + name)
+    # test search method
+    searchPoint = cloudpoint[50]
+    [ind_radius_search, sqdist_radius_search] = octreeNormal.radius_search(searchPoint, 30, 20)
+    print ('    radius search: {} results'.format(len(ind_radius_search)))
 
-# bounding box search
-[bmin, bmax] = octreeNormal.get_bounding_box()
-idx_3 = octreeNormal.box_search(bmin, bmax)
+    # nearest k search
+    [ind_k_search, sqdist_k_search] = octreeNormal.nearest_k_search_for_a_point(searchPoint, 20)
+    print ('    nearest k search: {} results'.format(len(ind_k_search)))
 
-# voxel search
-ind = octreeNormal.voxel_search(searchPoint)
+    # nearest point
+    [idx_nearest_search, dst_nearest_search] = octreeNormal.approx_nearest_search(cloudpoint[50]);
+    assert idx_nearest_search == 50
+    print ('    nearest search: {}'.format(idx_nearest_search))
 
-print ('Neighbors within voxel search at (' + str(searchPoint[0]) + ' ' + str(searchPoint[1]) + ' ' + str(searchPoint[2]) + ')')
-for i in range(0, ind.size):
-    print ('index = ' + str(ind[i]))
-    print ('(' + str(cloudpoint[ind[i]][0]) + ' ' + str(cloudpoint[ind[i]][1]) + ' ' + str(cloudpoint[ind[i]][2]))
+    # bounding box search
+    [bmin, bmax] = octreeNormal.get_bounding_box()
+    idx_bounding_search = octreeNormal.box_search(bmin, bmax)
+    print ('    bounding box search: {} results'.format(len(idx_bounding_search)))
+
+    # voxel search
+    idx_voxel_search = octreeNormal.voxel_search(searchPoint)
+    print ('    voxel search: {} results'.format(len(idx_voxel_search)))
+
+#for i in range(0, idx_voxel_search.size):
+#    print ('index = ' + str(idx_voxel_search[i]))
+#    print ('(' + str(cloudpoint[idx_voxel_search[i]][0]) + ' ' + str(cloudpoint[idx_voxel_search[i]][1]) + ' ' + str(cloudpoint[idx_voxel_search[i]][2]))
