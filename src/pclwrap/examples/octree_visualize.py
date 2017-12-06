cfc2c97bba68bfa71ebe30bc7e83d871dd0f4374
khanhha <khanhhh89@gmail.com>
2017-12-06 11:22:37 +0700

    draw octree leaf cubes

--- src/pclwrap/examples/octree_visualize.py
+++ src/pclwrap/examples/octree_visualize.py
@@ -42,17 +42,82 @@ def vtk_build_point_actor(points):
 
     return actor
 
+def vtk_build_box_actor(bounds):
+    box_offsets = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1))
+    box_edges = ((0,1), (0,2), (0,3), (4,7), (5,7), (6,7), (1,6), (2,5), (2,4), (3,6), (3,5), (1,4))
+
+    nbox = len(bounds)
+
+    vtk_pcoords = vtk.vtkFloatArray()
+    vtk_pcoords.SetNumberOfComponents(3)
+    vtk_pcoords.SetNumberOfTuples(nbox * 8)
+    for i in range(0, nbox):
+        bmin = bounds[i][0]
+        bmax = bounds[i][1]
+        delta = (bmax[0] - bmin[0], bmax[1] - bmin[1], bmax[2] - bmin[2])
+        for j in range(0,8):
+            x = bmin[0] + box_offsets[j][0]* delta[0]
+            y = bmin[1] + box_offsets[j][1]* delta[1]
+            z = bmin[2] + box_offsets[j][2]* delta[2]
+            vtk_pcoords.SetTuple3(i*8+j, x, y, z)
+
+    vtk_points = vtk.vtkPoints()
+    vtk_points.SetData(vtk_pcoords)
+
+    lines = vtk.vtkCellArray()
+    lines.Allocate(nbox * 12 * 2 + nbox * 12, 1000)
+
+    for i in range(0, nbox):
+        for e in range(0,12):
+            #line = vtk.vtkLine()
+            #line.GetPointIds().SetId(0, i*8 + box_edges[e][0])
+            #line.GetPointIds().SetId(1, i*8 + box_edges[e][1])
+            #lines.InsertNextCell(line)
+            lines.InsertNextCell(2, (i*8 + box_edges[e][0], i*8 + box_edges[e][1]))
+            #vertices.InsertCellPoint(i*8 + box_edges[e][0])
+            #vertices.InsertCellPoint(i*8 + box_edges[e][1])
+
+    vtk_poly = vtk.vtkPolyData()
+    vtk_poly.SetPoints(vtk_points)
+    vtk_poly.SetLines(lines)
+
+    ncell = vtk_poly.GetNumberOfCells()
+
+    # Visualize
+    mapper = vtk.vtkPolyDataMapper()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        mapper.SetInput(vtk_poly)
+    else:
+        mapper.SetInputData(vtk_poly)
+        mapper.Update()
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    return actor
+
 def vtk_build_octree_leaf_center_actor(octree):
     [keys, depths] = octreeNormal.get_all_leaf_keys()
     nleaf = len(keys)
     points = np.ndarray([nleaf, 3], dtype=float)
     for k in range(nleaf):
         c = octree.gen_voxel_center_from_octree_key(keys[k], depths[k])
-        points[k][0] = c[0]
-        points[k][1] = c[1]
-        points[k][2] = c[2]
+        for i in range(0,3):
+            points[k][i] = c[i]
 
     return vtk_build_point_actor(points)
+
+def vtk_build_octree_leaf_box_actor(octree):
+    [keys, depths] = octreeNormal.get_all_leaf_keys()
+    nleaf = len(keys)
+    bounds = np.ndarray([nleaf, 2, 3], dtype = float)
+    for k in range(nleaf):
+        [bmin, bmax] = octree.gen_voxel_bounds_from_octree_key(keys[k], depths[k])
+        for i in range(0,3):
+            bounds[k][0][i] = bmin[i]
+            bounds[k][1][i] = bmax[i]
+
+    return vtk_build_box_actor(bounds)
 
 def vtk_build_point_cloud_actor(cloud):
     npoints = cloud.size
@@ -104,6 +169,10 @@ vtk_leaf_center_actor = vtk_build_octree_leaf_center_actor(octreeNormal)
 vtk_leaf_center_actor.GetProperty().SetColor(0.2, 0.63, 0.79)
 vtk_leaf_center_actor.GetProperty().SetPointSize(2)
 
+vtk_leaf_box_actor = vtk_build_octree_leaf_box_actor(octreeNormal)
+vtk_leaf_box_actor.GetProperty().SetColor(0.9, 0.5, 0.3)
+vtk_leaf_box_actor.GetProperty().SetLineWidth(1)
+
 vtk_cloud_point_actor = vtk_build_point_cloud_actor(cloudpoint)
 
 camera = vtk.vtkCamera()
@@ -113,6 +182,7 @@ camera.SetFocalPoint(0, 0, 0)
 renderer = vtk.vtkRenderer()
 renderer.AddActor(vtk_leaf_center_actor)
 renderer.AddActor(vtk_cloud_point_actor)
+renderer.AddActor(vtk_leaf_box_actor)
 renderer.SetActiveCamera(camera)
 renderer.ResetCamera()
 renderer.SetBackground(0, 0, 0)
